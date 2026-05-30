45c98f7068fe0f58297fa4022059f1fd3db8eb3e
MahmoudAbdelaziz1 <ma7moud356845@gmail.com>
2026-05-30 13:35:02 +0300

    Update ProductImage.php

--- app/Models/ProductImage.php
+++ app/Models/ProductImage.php
@@ -52,6 +52,10 @@ class ProductImage extends Model
    
     public function getImageUrlAttribute($value)
     {
+        if (filter_var($value, FILTER_VALIDATE_URL)) {
+            return $value;
+        }
+        
         return asset('storage/' . $value);
     }
 }
