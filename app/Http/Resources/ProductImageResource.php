4be556597f638f5e8bd54388f7f06d89dfec9906
MahmoudAbdelaziz1 <ma7moud356845@gmail.com>
2026-06-11 14:07:53 +0300

    Update ProductImageResource.php

--- app/Http/Resources/ProductImageResource.php
+++ app/Http/Resources/ProductImageResource.php
@@ -10,17 +10,25 @@ class ProductImageResource extends JsonResource
     public function toArray($request)
     {
         $rawUrl = $this->image_url ? trim($this->image_url) : null;
+        $finalUrl = null;
+
+        if ($rawUrl) {
+           
+            if (preg_match('#/storage/(https?://.*)#i', $rawUrl, $matches)) {
+                $rawUrl = $matches[1];    
+            }
+
+            if (filter_var($rawUrl, FILTER_VALIDATE_URL)) {
+                $finalUrl = $rawUrl;  
+            } else {
+                $finalUrl = asset('storage/' . $rawUrl);   
+            }
+        }
 
         return [
             'image_id' => $this->image_id,
             'product_id' => $this->product_id,
-            
-            'image_url' => $rawUrl 
-                ? (filter_var($rawUrl, FILTER_VALIDATE_URL) 
-                    ? $rawUrl                     
-                    : asset('storage/' . $rawUrl))  
-                : null,
-                
+            'image_url' => $finalUrl,
             'alt_text' => $this->alt_text,
             'created_at' => $this->created_at ? $this->created_at->format('Y-m-d H:i:s') : null,
             'updated_at' => $this->updated_at ? $this->updated_at->format('Y-m-d H:i:s') : null,
